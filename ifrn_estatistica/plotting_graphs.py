26281b3a15eecac50af44f68f3d7cfc185e970d5
hadtrindade <haddleytrindade@gmail.com>
2021-04-18 11:22:08 -0300

    adicionado grafico para distribuiçao normal

--- ifrn_estatistica/plotting_graphs.py
+++ ifrn_estatistica/plotting_graphs.py
@@ -1,5 +1,11 @@
-from matplotlib import pyplot
 from typing import NoReturn, List
+from typing import Union
+from matplotlib import pyplot
+from scipy.stats import norm
+import numpy as np
+
+
+numeric = Union[int, float, complex]
 
 
 class PlottingGraphs:
@@ -22,10 +28,16 @@ class PlottingGraphs:
         plot.histogram_chart()
         plot.histogram_chart_bars()
         plot.pie_chart()
+        plot.normal_curve()
     """
 
     def __init__(
-        self, dataset: List, classes: List, percentages: List, fci: List
+        self,
+        dataset: List,
+        classes: List,
+        percentages: List,
+        fci: List,
+        keep: bool = None,
     ):
         """Inicia o obj para geração dos gráficos.
 
@@ -38,6 +50,7 @@ class PlottingGraphs:
         self.classes = classes
         self.percentages = percentages
         self.fci = fci
+        self.keep = keep
 
     def _class_names(self):
 
@@ -54,7 +67,10 @@ class PlottingGraphs:
         name_classes = self._class_names()
         pyplot.hist(self.dataset, bins=len(name_classes), rwidth=0.95)
         pyplot.title("HISTOGRAMA DE FREQUENCIA")
-        pyplot.show()
+        if self.keep:
+            pyplot.savefig("histogram_chart.png")
+        else:
+            pyplot.show()
 
     def simple_graph(self) -> NoReturn:
         """Método para geração de grafico simples.
@@ -72,7 +88,10 @@ class PlottingGraphs:
         )
         pyplot.title("FPI & FCI")
         pyplot.legend()
-        pyplot.show()
+        if self.keep:
+            pyplot.savefig("simple_graph.png")
+        else:
+            pyplot.show()
 
     def pie_chart(self) -> NoReturn:
         """Método para geração de grafico pizza.
@@ -88,7 +107,10 @@ class PlottingGraphs:
             loc="center left",
             bbox_to_anchor=(1, 0, 0.5, 1),
         )
-        pyplot.show()
+        if self.keep:
+            pyplot.savefig("pie_chart.png")
+        else:
+            pyplot.show()
 
     def histogram_chart_bars(self) -> NoReturn:
         """Método para geração de grafico histograma com barras.
@@ -112,4 +134,36 @@ class PlottingGraphs:
         pyplot.xlabel("Classes")
         pyplot.legend()
         pyplot.ylabel("Porcentagem")
-        pyplot.show()
+        if self.keep:
+            pyplot.savefig("histogram_table.png")
+        else:
+            pyplot.show()
+
+    def distribution_chart(
+        self, title="", z1: numeric = 0, z2: numeric = 0,
+    ) -> NoReturn:
+
+        x = np.arange(z1, z2, 0.001)
+        x_normal_curve = np.arange(-10, 10, 0.001)
+        y_probility = norm.pdf(x, 0, 1)
+        y_normal_curve = norm.pdf(x_normal_curve, 0, 1)
+        _, ax = pyplot.subplots(figsize=(9, 6))
+        pyplot.style.use("fivethirtyeight")
+        ax.plot(x_normal_curve, y_normal_curve)
+        ax.fill_between(x, y_probility, 0, alpha=0.3, color="b")
+        ax.fill_between(x_normal_curve, y_normal_curve, 0, alpha=0.1)
+        ax.set_xlim([-4, 4])
+        ax.set_xlabel("Desvio Padrão")
+        ax.set_yticklabels([])
+        ax.set_title(f"Distribuição Normal para {title}")
+        pyplot.legend(
+            [],
+            title="Medidas de Dispersão",
+            loc="upper right",
+            #bbox_to_anchor=(1, 0, 0.5, 1),
+        )
+
+        if self.keep:
+            pyplot.savefig("normal_curve.png", dpi=72, bbox_inches="tight")
+        else:
+            pyplot.show()
